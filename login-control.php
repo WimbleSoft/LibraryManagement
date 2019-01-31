72364a2d57cf73d4ac5ebc3558180b93068a8a6a
Fatih Sağlam <m.fatihsaglam@outlook.com>
2019-01-31 04:00:29 +0300

    Update login-control.php

--- login-control.php
+++ login-control.php
@@ -7,7 +7,7 @@ $passWord 	= md5($_POST['passWord']);
 	if ((!$userName =="") and (!$passWord =="")) {
 		
 		$sql = $connection->prepare("select * from personnel where userName='$userName' and passWord='$passWord' ");
-		$query = $connection->query("select * from personnel where userName='{$userName}' and passWord='{$passWord}'  ")->fetchAll(PDO::FETCH_ASSOC);
+		$query = $connection->query("select * from personnel where userName='$userName' and passWord='$passWord'  ")->fetchAll(PDO::FETCH_ASSOC);
 		$sql->execute();
 		
 		
@@ -16,10 +16,8 @@ $passWord 	= md5($_POST['passWord']);
 		
 			foreach ($query as $pulledData)
 			{
-			$_SESSION["enteredPerson"] = $pulledData['nameSurname'];
-			$_SESSION["enteredPersonId"] = $pulledData['id'];
-			$_SESSION["enteredPersonEmail"] = $pulledData['eMail'];
-			$_SESSION["auth"] = $pulledData['auth'];
+			$_SESSION["userName"] = $pulledData['userName'];
+			$_SESSION["personnelId"] = $pulledData['personnelId'];
 			}
 			
 			@header ("Location: index.php");
